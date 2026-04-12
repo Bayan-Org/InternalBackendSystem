627e67ed48877e009f20e18276d45daf705565fd
Dzikri-Ardafi <dzkr.ardfi@gmail.com>
2026-04-12 17:05:21 +0700

    feat: add console for indicate where server running

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import config from "./configs/dotenv-config.js";
 import "./configs/init-config.js";
 
 app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
+  console.log(`Server running at http://localhost:${config.port}`);
 });
