e61066a23dd5c7b603b1f32c22614c83fc174d71
Bayan-DzikriArdafi <dzikri.ardafi@bayan.com.sg>
2026-04-23 23:35:48 +0700

    add logging

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Incoming Before Cors:", req.method, req.url);
+  next();
+});
+
 // 🔥 HARUS PALING ATAS
 app.use(cors())
 
 app.use((req, res, next) => {
-  console.log("Incoming:", req.method, req.url);
+  console.log("Incoming After Cors:", req.method, req.url);
   next();
 });
 // app.use(cors({
