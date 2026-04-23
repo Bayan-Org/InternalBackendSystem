96dfa78117ae6c3b4c057d547394f7b287808f86
Bayan-DzikriArdafi <dzikri.ardafi@bayan.com.sg>
2026-04-23 23:26:49 +0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -7,22 +7,23 @@ const app = express();
 app.use(express.json());
 
 // 🔥 HARUS PALING ATAS
-app.use(cors({
-  origin: "https://dev-ecology.bayan.com.sg",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin"]
-}));
+app.use(cors())
+// app.use(cors({
+//   origin: "https://dev-ecology.bayan.com.sg",
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin"]
+// }));
 
-// 🔥 handle preflight secara manual (Express v5 safe)
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", "https://dev-ecology.bayan.com.sg");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    return res.sendStatus(200);
-  }
-  next();
-});
+// // 🔥 handle preflight secara manual (Express v5 safe)
+// app.use((req, res, next) => {
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Origin", "https://dev-ecology.bayan.com.sg");
+//     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 
 
 app.get("/", (req: Request, res: Response) => {
