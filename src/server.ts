bebd5908c8b6ac7dbacc79f60c4b00a97fdc614c
Bayan-DzikriArdafi <dzikri.ardafi@bayan.com.sg>
2026-04-23 23:13:48 +0700

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import app from "./app.js";
 import config from "./configs/dotenv-config.js";
 import "./configs/init-config.js";
+import cors from "cors";
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
+
 
 app.listen(config.port, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${config.port}`);
