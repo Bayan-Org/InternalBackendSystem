609c575011adfbbf75325f48a6495ae4737ce8f9
Bayan-DzikriArdafi <dzikri.ardafi@bayan.com.sg>
2026-04-23 23:22:14 +0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
 import express, { type Request, type Response } from "express";
 import AuthRouter from "./routes/auth-route.js";
 import UserRouter from "./routes/user-route.js";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+
+// 🔥 HARUS PALING ATAS
+app.use(cors({
+  origin: "https://dev-ecology.bayan.com.sg",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+// 🔥 handle preflight secara manual (Express v5 safe)
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "https://dev-ecology.bayan.com.sg");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 
 app.get("/", (req: Request, res: Response) => {
   res.status(201).json({
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
 import app from "./app.js";
 import config from "./configs/dotenv-config.js";
 import "./configs/init-config.js";
-import cors from "cors";
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
 
 
 
