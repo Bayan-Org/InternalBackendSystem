9b19276bd07527327609f1d50f70c4091e34ffca
Bayan-DzikriArdafi <dzikri.ardafi@bayan.com.sg>
2026-04-23 20:30:53 +0700

    fix

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -2,24 +2,22 @@ import axios from "axios";
 import type { Response, Request } from "express";
 
 export const getProfileHandler = async (req: Request, res: Response) => {
-
-  const accessToken = req.headers.authorization;
-  console.log(accessToken);
+  const accessToken = req.headers.authorization || process.env.ACCESS_TOKEN;
   const BASE_APP_URL = process.env.BASE_APP_URL;
   
+
   const requestURL = `${BASE_APP_URL}/odata/v4/current-user/ZC_GET_CURRENT_USER`;
   try {
-    // const response = await axios.get(requestURL, {
-    //   headers: {
-    //     Authorization: `Bearer ${accessToken}`,
-    //   },
-    // });
+    const response = await axios.get(requestURL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
     
     return res.status(201).json({
       message: "Success",
       statusCode: 201,
-      userEmail: "dzikri.ardafi@bayan.com.sg",
-      accessToken
+      response: response.data.value[0].UserEmail,
     });
   } catch (error) {
     return res.json({
