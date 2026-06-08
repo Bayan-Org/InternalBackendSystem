2f5b9319ab9060acb0de17e93eb5450c14f91f07
Bayan-DzikriArdafi <dzikri.ardafi@bayan.com.sg>
2026-06-08 13:16:19 +0700

    check

--- src/app.ts
+++ src/app.ts
@@ -73,7 +73,7 @@ app.use(express.json());
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({
     success: true,
-    message: "Server running",
+    message: "Health",
   });
 });
 
