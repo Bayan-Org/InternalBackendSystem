0c4e05658575daf8f856bda3300cdc36e5fc2734
Bayan-DzikriArdafi <dzikri.ardafi@bayan.com.sg>
2026-06-09 11:25:39 +0700

    ok

--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -5,8 +5,10 @@ import {
   loginHandler,
   logoutHandler,
   refreshTokenHandler,
+  verifyToken,
 } from "../controllers/auth-controller.js";
 import { loginLimiter } from "../middleware/rate-limit-middleware.js";
+import { AuthMiddleware } from "../middleware/auth-middleware.js";
 
 const AuthRouter = express.Router();
 
@@ -15,6 +17,7 @@ AuthRouter.get("/logout", logoutHandler);
 AuthRouter.get("/good-bye", (req, res) => {
   res.send("See you when i see you again");
 });
+AuthRouter.get("/verify-token", AuthMiddleware, verifyToken);
 AuthRouter.get("/callback", callbackHandler);
 AuthRouter.get("/exchange", exchangeHandler);
 AuthRouter.post("/refresh-token", refreshTokenHandler);
